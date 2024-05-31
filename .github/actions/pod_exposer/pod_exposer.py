5f3bb21b96e237ce4f4ef3921f79139e05470222
jnissan1 <yoni.nissan@gmail.com>
2024-05-31 20:09:43 +0300

    removed the prints, might be the issue.

--- .github/actions/pod_exposer/pod_exposer.py
+++ .github/actions/pod_exposer/pod_exposer.py
@@ -23,19 +23,15 @@ class PodExposer(Base):
 
     def run(self):
         
-    #    print(self.pod_name)
-    #    print(f"inside def run")
         if self.pod_name is not None:
             print(self.pod_name)
         else:
             raise Exception("Failed to get the runner pod's name.")
 
     def on_exception(self, e):
-        print(f"inside def on_exception")
         raise Exception from e
 
     def on_end(self):
-        print(f"inside def on_end")
         pass
 
 
